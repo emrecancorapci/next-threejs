b451e19282983c65cea425063816bd2ea174aef0
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-09-15 21:26:42 +0300

    add box and floor

---
+++ src/app/(three)/basic/_components/box.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import type { Mesh } from 'three';
+
+import type { MeshProperties } from '@/app/types/MeshProperties';
+
+export default function Box(properties: MeshProperties) {
+  const reference = useRef<Mesh>();
+
+  useFrame((state, delta) => {
+    return (reference.current.rotation.x += delta);
+  });
+
+  return (
+    <mesh {...properties} ref={reference}>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <boxGeometry args={[1, 1, 1]} />
+      <meshPhysicalMaterial color="red" />
+    </mesh>
+  );
+}
---
+++ src/app/(three)/basic/_components/floor.tsx
@@ -0,0 +1,11 @@
+import { type MeshProperties } from '@/app/types/MeshProperties';
+
+export default function Floor(properties: MeshProperties) {
+  return (
+    <mesh {...properties}>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <boxGeometry args={[5, 0.1, 5]} />
+      <meshPhysicalMaterial color="green" />
+    </mesh>
+  );
+}
