46b1a9eb688e3a829de97d24baa68402f0ea3a51
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-09-16 20:24:09 +0300

    add leva to layout

--- src/app/(three)/layout.tsx
+++ src/app/(three)/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Canvas } from '@react-three/fiber';
+import { Leva } from 'leva';
 
 interface Properties {
   children: React.ReactNode;
@@ -8,8 +9,11 @@ interface Properties {
 
 export default function Page({ children }: Properties) {
   return (
-    <Canvas shadows={true} camera={{ position: [0, 5, 7] }} className="h-screen w-screen bg-black">
-      {children}
-    </Canvas>
+    <>
+      <Canvas shadows={true} camera={{ position: [0, 5, 7] }} className="h-screen w-screen bg-zinc-700">
+        {children}
+      </Canvas>
+      <Leva collapsed={true} />
+    </>
   );
 }
