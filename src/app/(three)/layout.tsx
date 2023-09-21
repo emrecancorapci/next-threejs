197737133decf780c63e0686f2b64e1dbaf63537
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-09-21 16:54:23 +0300

    add keyboard controls

--- src/app/(three)/layout.tsx
+++ src/app/(three)/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { KeyboardControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Leva } from 'leva';
 
@@ -9,11 +10,19 @@ interface Properties {
 
 export default function Page({ children }: Properties) {
   return (
-    <>
+    <KeyboardControls
+      map={[
+        { name: 'forward', keys: ['ArrowUp', 'w', 'W'] },
+        { name: 'backward', keys: ['ArrowDown', 's', 'S'] },
+        { name: 'left', keys: ['ArrowLeft', 'a', 'A'] },
+        { name: 'right', keys: ['ArrowRight', 'd', 'D'] },
+        { name: 'jump', keys: ['Space'] },
+      ]}
+    >
       <Canvas shadows={true} camera={{ position: [0, 5, 7] }} className="h-screen w-screen bg-zinc-700">
         {children}
       </Canvas>
       <Leva collapsed={true} />
-    </>
+    </KeyboardControls>
   );
 }
