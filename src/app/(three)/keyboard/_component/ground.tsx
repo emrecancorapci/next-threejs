9f03d1375c41ec888c174a0d019284225bf2330b
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-09-21 16:54:59 +0300

    add ground for keyboard page

---
+++ src/app/(three)/keyboard/_component/ground.tsx
@@ -0,0 +1,17 @@
+import { useTexture } from '@react-three/drei';
+import { CuboidCollider, RigidBody } from '@react-three/rapier';
+import * as THREE from 'three';
+
+export default function Ground() {
+  const texture = useTexture('textures/grass.jpg');
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  return (
+    <RigidBody type="fixed" colliders={false}>
+      <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[1000, 1000]} />
+        <meshStandardMaterial map={texture} map-repeat={[240, 240]} color="green" />
+      </mesh>
+      <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
+    </RigidBody>
+  );
+}
