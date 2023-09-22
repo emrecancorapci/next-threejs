af4730a5b50dad9ec2148e9047e9480b029f2cc6
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-09-22 18:39:15 +0300

    add kayboard movement

--- src/app/(three)/keyboard/page.tsx
+++ src/app/(three)/keyboard/page.tsx
@@ -1,23 +1,44 @@
 'use client';
 
-import { OrbitControls, Sky } from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
-import { Physics } from '@react-three/rapier';
-import { useEffect } from 'react';
+import { OrbitControls, Sky, useKeyboardControls } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
+import { Physics, type RapierRigidBody, RigidBody } from '@react-three/rapier';
+import { useEffect, useRef } from 'react';
+import { type PerspectiveCamera, Vector3 } from 'three';
 
 import Box from '@/components/box';
 
 import Ground from './_component/ground';
 
+const SPEED = 5;
+const direction = new Vector3();
+const frontVector = new Vector3();
+const sideVector = new Vector3();
+
 export default function Keyboard() {
   const three = useThree();
-  const cam = three.camera as THREE.PerspectiveCamera;
+  const [, get] = useKeyboardControls();
+  const boxReference = useRef<RapierRigidBody>();
+  const cam = three.camera as PerspectiveCamera;
 
   useEffect(() => {
     cam.position.set(0, 5, 5);
     cam.fov = 45;
     console.log('cam', cam);
   }, [cam]);
+
+  useFrame((state) => {
+    const { forward, backward, left, right } = get();
+
+    // @ts-expect-error - Ts doesn't like the fact that we extract booleans. But who cares? I won't take orders from a computer.
+    const [horizontalScalar, verticalScalar] = [left - right, backward - forward];
+
+    frontVector.set(0, 0, verticalScalar);
+    sideVector.set(horizontalScalar, 0, 0);
+    direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED);
+
+    boxReference.current?.setLinvel(direction, true);
+  });
 
   return (
     <>
@@ -26,11 +47,21 @@ export default function Keyboard() {
       <ambientLight intensity={0.3} />
 
       <Physics>
-        <Box position={[0, 1, 0]} />
+        <RigidBody
+          // @ts-expect-error - Ts doesn't like the reference type.
+          ref={boxReference}
+          colliders={false}
+          mass={1}
+          type="dynamic"
+          position={[0, 1, 0]}
+          enabledRotations={[false, false, false]}
+        >
+          <Box />
+        </RigidBody>
         <Ground />
       </Physics>
 
-      <OrbitControls autoRotate autoRotateSpeed={4} enablePan={false} enableZoom={true} />
+      <OrbitControls enablePan={false} enableZoom={true} />
 
       {/* <Environment preset="warehouse" /> */}
     </>
