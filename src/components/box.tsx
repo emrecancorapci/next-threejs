b929a99a444fc0e207ac8d9832f04870e8fdf323
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-09-28 21:27:38 +0300

    add children to box

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -5,11 +5,12 @@ import type { Mesh } from 'three';
 
 import type { MeshProperties } from '@/types/MeshProperties';
 
-const Box = forwardRef<Mesh, MeshProperties>(({ position, roughness, metalness, color, size }, reference) => {
+const Box = forwardRef<Mesh, MeshProperties>(({ position, roughness, metalness, color, size, children }, reference) => {
   return (
     <mesh castShadow receiveShadow position={position} ref={reference}>
       <boxGeometry args={size} />
       <meshPhysicalMaterial roughness={roughness} metalness={metalness} color={color ?? 'white'} />
+      {children}
     </mesh>
   );
 });
